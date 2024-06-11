8e685813acf02163695cadd90ab1008eb643b171
nkondra77999 <nkondra77999@gmail.com>
2024-06-11 12:51:43 +0530

    create subnets

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,35 @@ resource "aws_vpc" "lms-vpc" {
     Name = "lms-vpc"
   }
 }
+#web Subnet 
+resource "aws_subnet" "lms-web-subnet" {
+  vpc_id     = aws_vpc.lms-vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+  tags = {
+    Name = "lms-web-subnet"
+  }
+}
+
+#api subnet
+resource "aws_subnet" "lms-api-subnet" {
+  vpc_id     = aws_vpc.lms-vpc.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "lms-api-subnet"
+  }
+}
+
+#Db subnet
+
+resource "aws_subnet" "lms-db-subnet" {
+  vpc_id     = aws_vpc.lms-vpc.id
+  cidr_block = "10.0.3.0/24"
+  #map_public_ip_on_launch = "false"
+
+  tags = {
+    Name = "lms-db-subnet"
+  }
+}
