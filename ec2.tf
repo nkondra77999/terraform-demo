06240f5b5db3c65b7ab19f1742acfe6d8b2b8551
nkondra77999 <nkondra77999@gmail.com>
2024-06-11 14:58:21 +0530

    update sg

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "lms-web-server" {
   instance_type = "t2.micro"
   subnet_id   = aws_subnet.lms-web-subnet.id
   key_name = "Virginia"
-  vpc_security_group_ids = aws_security_group.lms-web-sg.id
+  vpc_security_group_ids = [aws_security_group.lms-web-sg.id]
   user_data = file("setup.sh")
   tags = {
     Name = "lms-web-server"
